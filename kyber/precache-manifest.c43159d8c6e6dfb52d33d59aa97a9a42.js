eba25a65bf3011aa81e932ddf896c96829c0341b
Egor Antonov <glycin@inbox.ru>
2022-01-18 00:30:15 +0300

    - added Kyber

---
+++ kyber/precache-manifest.c43159d8c6e6dfb52d33d59aa97a9a42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32f018cec9c09f9e557692fc508120bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66efb2058bca09ad9cdb",
+    "url": "/static/css/main.815af142.chunk.css"
+  },
+  {
+    "revision": "1789a2f80ef4f47087f7",
+    "url": "/static/js/2.78a62871.chunk.js"
+  },
+  {
+    "revision": "66efb2058bca09ad9cdb",
+    "url": "/static/js/main.9a843c15.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
